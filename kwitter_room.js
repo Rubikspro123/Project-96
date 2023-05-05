b9c6158c3938ce0c2e12a59eac89213b80a7fa4d
Rubikspro123 <87181104+Rubikspro123@users.noreply.github.com>
2023-05-05 17:06:25 +0700

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,16 +1,16 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-      apiKey: "AIzaSyDbfe7sjbfY3ack_iLAEp84QWoiQ_CYiQY",
-      authDomain: "kwitterakshit.firebaseapp.com",
-      databaseURL: "https://kwitterakshit-default-rtdb.firebaseio.com",
-      projectId: "kwitterakshit",
-      storageBucket: "kwitterakshit.appspot.com",
-      messagingSenderId: "41193646220",
-      appId: "1:41193646220:web:367aa3a021206acb61f3a9"
-    };
-    
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDbfe7sjbfY3ack_iLAEp84QWoiQ_CYiQY",
+  authDomain: "kwitterakshit.firebaseapp.com",
+  databaseURL: "https://kwitterakshit-default-rtdb.firebaseio.com",
+  projectId: "kwitterakshit",
+  storageBucket: "kwitterakshit.appspot.com",
+  messagingSenderId: "41193646220",
+  appId: "1:41193646220:web:367aa3a021206acb61f3a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 //ADD YOUR FIREBASE LINKS HERE
 user_name = localStorage.getItem("user_name");
 room_name = localStorage.getItem("room_name");
