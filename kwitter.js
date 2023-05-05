d55ad6f9a320677178e985072ba446bdc727a5e3
Rubikspro123 <87181104+Rubikspro123@users.noreply.github.com>
2023-05-05 17:44:16 +0700

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,3 +1,17 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBIvCxeRPnlncOydplvLXIDAdQS1QJpv6s",
+    authDomain: "kwitter-revive.firebaseapp.com",
+    databaseURL: "https://kwitter-revive-default-rtdb.firebaseio.com",
+    projectId: "kwitter-revive",
+    storageBucket: "kwitter-revive.appspot.com",
+    messagingSenderId: "294593029594",
+    appId: "1:294593029594:web:6d8181bcbac2e88ff8cce3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
 function addUser(){
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name" , user_name);
